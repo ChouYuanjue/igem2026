bfc77949240126ae747ee084b852eda2445421b2
ChouYuanjue <2700074128@qq.com>
2026-09-02 12:06:52 +0000

    Freeze TPS foundation R2E search

--- projects/active/terpene_screening/tests/test_open_world_tps_temporal_contracts.py
+++ projects/active/terpene_screening/tests/test_open_world_tps_temporal_contracts.py
@@ -18,3 +18,13 @@ def test_tps_temporal_contract_uses_official_versions_and_separate_general_tps_a
  assert p['cells']['legacy_frozen_double_cold']
  assert p['baselines']['general_model_reference'] != p['baselines']['tps_internal_reference']
  assert p['minimum_support']['temporal_strict_double_cold_queries']==20
+
+def test_tps_foundation_r2e_search_is_small_and_development_only():
+ p=json.loads((ROOT/'projects/active/terpene_screening/CATALYST_TPS_FOUNDATION_R2E_V1.json').read_text())
+ assert p['status']=='frozen_before_new_foundation_feature_development_scores'
+ assert len(p['candidates'])==7
+ assert p['feature_policy']['clean2023_retrieval_weights_allowed'] is False
+ assert p['feature_policy']['v2_1_labels_or_performance_allowed'] is False
+ assert p['split_semantics']['selection_may_read_legacy_frozen'] is False
+ assert p['development_evaluation']['minimum_hit10_absolute_gain_to_open_legacy_frozen']==0.02
+ assert p['legacy_frozen_policy']['at_most_one_new_candidate'] is True
